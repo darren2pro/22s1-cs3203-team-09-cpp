b6d98d3ac9ad282bbce188f0aa3c485c68d1015d
darren2pro <darren2pro@gmail.com>
2022-08-25 16:03:54 +0800

    Add unit test for poor format SIMPLE code

--- Team09/Code09/UnitTesting/TestTokenizer.cpp
+++ Team09/Code09/UnitTesting/TestTokenizer.cpp
@@ -12,7 +12,7 @@ namespace UnitTesting {
     TEST_CLASS(TestTokenizer) {
             public:
 
-            TEST_METHOD(TestTokenize) {
+            TEST_METHOD(TestTokenizeNiceFormat) {
                 const std::string str = "procedure computeAverage {\n"
                                         "    read num1;\n"
                                         "    read num2;\n"
@@ -21,7 +21,44 @@ namespace UnitTesting {
                                         "\n"
                                         "    print sum;\n"
                                         "}";
-                Logger::WriteMessage(("Program to test:\n" + str).c_str());
+                vector<SimpleToken> expectedResult;
+                expectedResult.push_back(SimpleToken("procedure"));
+                expectedResult.push_back(SimpleToken("computeAverage"));
+                expectedResult.push_back(SimpleToken("{"));
+                expectedResult.push_back(SimpleToken("read"));
+                expectedResult.push_back(SimpleToken("num1"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("read"));
+                expectedResult.push_back(SimpleToken("num2"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("sum"));
+                expectedResult.push_back(SimpleToken("="));
+                expectedResult.push_back(SimpleToken("num1"));
+                expectedResult.push_back(SimpleToken("+"));
+                expectedResult.push_back(SimpleToken("num2"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("print"));
+                expectedResult.push_back(SimpleToken("sum"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("}"));
+
+                SimpleParser simpleParser(str);
+                Parser* parser = &simpleParser;
+                vector<SimpleToken> result = parser->getTokens();
+                Assert::AreEqual(expectedResult.size(), result.size());
+                for (int i = 0; i < expectedResult.size(); i++) {
+                    Assert::AreEqual(expectedResult[i].getValue(), result[i].getValue());
+                }
+            }
+
+            TEST_METHOD(TestTokenizePoorFormat) {
+                const std::string str = "procedure computeAverage{\n"
+                                        "    read num1;\n"
+                                        "    read num2;\n"
+                                        "\n"
+                                        "    sum= num1+num2;\n"
+                                        "\n"
+                                        "    print sum;}";
                 vector<SimpleToken> expectedResult;
                 expectedResult.push_back(SimpleToken("procedure"));
                 expectedResult.push_back(SimpleToken("computeAverage"));

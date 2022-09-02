b6cd8068db1c9b5b40d3e0f1a0be5c1fb35e8fbe
darren2pro <darren2pro@gmail.com>
2022-09-02 17:47:36 +0800

    Add tokenizer test for relational operators

--- Team09/Code09/UnitTesting/TestTokenizer.cpp
+++ Team09/Code09/UnitTesting/TestTokenizer.cpp
@@ -88,5 +88,91 @@ namespace UnitTesting {
                     Assert::AreEqual(expectedResult[i].getValue(), result[i].getValue());
                 }
             }
+
+            TEST_METHOD(TestTokenizeEmptyString) {
+                const std::string str = "";
+                vector<SimpleToken> expectedResult;
+                SimpleParser simpleParser(str);
+                Parser* parser = &simpleParser;
+                vector<SimpleToken> result = parser->getTokens();
+                Assert::AreEqual(expectedResult.size(), result.size());
+                for (int i = 0; i < expectedResult.size(); i++) {
+                    Assert::AreEqual(expectedResult[i].getValue(), result[i].getValue());
+                }
+            }
+
+            TEST_METHOD(TestTokenizeRelationalOperators) {
+                const std::string str = "procedure computeCentroid { \n"
+                                        "count = 0; \n"
+                                        "while ((x != 0) && (y != 0)) {\n"
+                                        "count = count + 1;\n"
+                                        "}\n"
+                                        "if (count == 0) then {\n"
+                                        "flag = 1;\n"
+                                        "} else {\n"
+                                        "cenX = cenX / count;\n"
+                                        "}\n"
+                                        "}";
+                vector<SimpleToken> expectedResult;
+                expectedResult.push_back(SimpleToken("procedure"));
+                expectedResult.push_back(SimpleToken("computeCentroid"));
+                expectedResult.push_back(SimpleToken("{"));
+                expectedResult.push_back(SimpleToken("count"));
+                expectedResult.push_back(SimpleToken("="));
+                expectedResult.push_back(SimpleToken("0"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("while"));
+                expectedResult.push_back(SimpleToken("("));
+                expectedResult.push_back(SimpleToken("("));
+                expectedResult.push_back(SimpleToken("x"));
+                expectedResult.push_back(SimpleToken("!="));
+                expectedResult.push_back(SimpleToken("0"));
+                expectedResult.push_back(SimpleToken(")"));
+                expectedResult.push_back(SimpleToken("&&"));
+                expectedResult.push_back(SimpleToken("("));
+                expectedResult.push_back(SimpleToken("y"));
+                expectedResult.push_back(SimpleToken("!="));
+                expectedResult.push_back(SimpleToken("0"));
+                expectedResult.push_back(SimpleToken(")"));
+                expectedResult.push_back(SimpleToken(")"));
+                expectedResult.push_back(SimpleToken("{"));
+                expectedResult.push_back(SimpleToken("count"));
+                expectedResult.push_back(SimpleToken("="));
+                expectedResult.push_back(SimpleToken("count"));
+                expectedResult.push_back(SimpleToken("+"));
+                expectedResult.push_back(SimpleToken("1"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("}"));
+                expectedResult.push_back(SimpleToken("if"));
+                expectedResult.push_back(SimpleToken("("));
+                expectedResult.push_back(SimpleToken("count"));
+                expectedResult.push_back(SimpleToken("=="));
+                expectedResult.push_back(SimpleToken("0"));
+                expectedResult.push_back(SimpleToken(")"));
+                expectedResult.push_back(SimpleToken("then"));
+                expectedResult.push_back(SimpleToken("{"));
+                expectedResult.push_back(SimpleToken("flag"));
+                expectedResult.push_back(SimpleToken("="));
+                expectedResult.push_back(SimpleToken("1"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("}"));
+                expectedResult.push_back(SimpleToken("else"));
+                expectedResult.push_back(SimpleToken("{"));
+                expectedResult.push_back(SimpleToken("cenX"));
+                expectedResult.push_back(SimpleToken("="));
+                expectedResult.push_back(SimpleToken("cenX"));
+                expectedResult.push_back(SimpleToken("/"));
+                expectedResult.push_back(SimpleToken("count"));
+                expectedResult.push_back(SimpleToken(";"));
+                expectedResult.push_back(SimpleToken("}"));
+                expectedResult.push_back(SimpleToken("}"));
+                SimpleParser simpleParser(str);
+                Parser* parser = &simpleParser;
+                vector<SimpleToken> result = parser->getTokens();
+                Assert::AreEqual(expectedResult.size(), result.size());
+                for (int i = 0; i < expectedResult.size(); i++) {
+                    Assert::AreEqual(expectedResult[i].getValue(), result[i].getValue());
+                }
+            }
     };
 }

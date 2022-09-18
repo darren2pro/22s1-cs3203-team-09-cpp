b5c1bdc85c9bcf1a420f99b6841489a748b6e958
darren2pro <darren2pro@gmail.com>
2022-09-19 01:23:30 +0800

    Comment out test cases
    Accidentally uncomment

--- Team09/Code09/UnitTesting/QueryProcessingSubsystem/Executor/TestExecute.cpp
+++ Team09/Code09/UnitTesting/QueryProcessingSubsystem/Executor/TestExecute.cpp
@@ -170,24 +170,24 @@ namespace UnitTesting {
 //                Assert::IsTrue(results3.find("p") != results3.end());
 
                 // Query 4
-                string query4 = "assign myAssign; variable myVar;\n"
-                                "Select myVar such that Modifies(\"p\", myVar) pattern myAssign(myVar, _)";
-                Query* q4 = QueryBuilder().buildQuery(query4);
-                unordered_set<string> results4 = executor.processQuery(q4);
-                Assert::IsTrue(results4.size() == 3, L"Query 4 fails");
-                // Expected results: x, i, z
-                Assert::IsTrue(results4.find("x") != results4.end());
-                Assert::IsTrue(results4.find("i") != results4.end());
-                Assert::IsTrue(results4.find("z") != results4.end());
+//                string query4 = "assign myAssign; variable myVar;\n"
+//                                "Select myVar such that Modifies(\"p\", myVar) pattern myAssign(myVar, _)";
+//                Query* q4 = QueryBuilder().buildQuery(query4);
+//                unordered_set<string> results4 = executor.processQuery(q4);
+//                Assert::IsTrue(results4.size() == 3, L"Query 4 fails");
+//                // Expected results: x, i, z
+//                Assert::IsTrue(results4.find("x") != results4.end());
+//                Assert::IsTrue(results4.find("i") != results4.end());
+//                Assert::IsTrue(results4.find("z") != results4.end());
 
                 // Query 5
-                string query5 = "proc procproc;"
-                                "Select procproc such that Modifies(\"p\", _)";
-                Query* q5 = QueryBuilder().buildQuery(query5);
-                unordered_set<string> results5 = executor.processQuery(q5);
-                Assert::IsTrue(results5.size() == 1, L"Query 5 fails");
-                // Expected results: p
-                Assert::IsTrue(results5.find("p") != results5.end());
+//                string query5 = "proc procproc;"
+//                                "Select procproc such that Modifies(\"p\", _)";
+//                Query* q5 = QueryBuilder().buildQuery(query5);
+//                unordered_set<string> results5 = executor.processQuery(q5);
+//                Assert::IsTrue(results5.size() == 1, L"Query 5 fails");
+//                // Expected results: p
+//                Assert::IsTrue(results5.find("p") != results5.end());
 
                 // Query 6
                 string query6 = "stmt ss;"

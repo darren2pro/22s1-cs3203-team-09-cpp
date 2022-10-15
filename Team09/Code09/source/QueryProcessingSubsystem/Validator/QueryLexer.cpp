85c2b9a272134f859a61c2ef552864b9fee7e2fd
rehad-a <rahaad@hotmail.com>
2022-10-15 12:56:58 +0800

    modify QueryLexer.cpp

--- Team09/Code09/source/QueryProcessingSubsystem/Validator/QueryLexer.cpp
+++ Team09/Code09/source/QueryProcessingSubsystem/Validator/QueryLexer.cpp
@@ -8,35 +8,39 @@
 
 namespace lexerre {
     std::regex whitespace("[ \t\n]+");
-    std::regex punctuation("[\(\),;_\.=]");
+    std::regex punctuation("[\(\),;_\.=<>]");
     std::regex allowed_charas("[A-Za-z0-9-/%\+\*#]");
 }
 
 std::vector<std::string> QueryLexer::lex() {
     std::vector<std::string> tokens;
-    
+
     std::string str = "";
 
-	for (int i = 0; i < query_string.length(); i++) {
+    for (int i = 0; i < query_string.length(); i++) {
         const char currentChar = query_string[i];
         if (std::regex_match(std::string(1, currentChar), lexerre::whitespace)) {
             if (str.length() > 0 && str[0] != '"') {
                 tokens.push_back(str);
                 str.clear();
             }
-        } else if (std::regex_match(std::string(1, currentChar), lexerre::punctuation)) {
+        }
+        else if (std::regex_match(std::string(1, currentChar), lexerre::punctuation)) {
             if (str.length() > 0) {
                 if (str[0] == '"') {    // check if it's part of an expression
                     str += currentChar;
-                } else {
+                }
+                else {
                     tokens.push_back(str);
                     tokens.push_back(std::string(1, currentChar));
                     str.clear();
                 }
-            } else {
+            }
+            else {
                 tokens.push_back(std::string(1, currentChar));
             }
-        } else if (currentChar == '"') {
+        }
+        else if (currentChar == '"') {
             if (str.length() > 0 && str[0] == '"') {
                 str += currentChar;
                 tokens.push_back(str);
@@ -44,16 +48,18 @@ std::vector<std::string> QueryLexer::lex() {
                 continue;
             }
             str += currentChar;
-        } else if (std::regex_match(std::string(1, currentChar), lexerre::allowed_charas)) {
-             str += currentChar;
-        } else {
+        }
+        else if (std::regex_match(std::string(1, currentChar), lexerre::allowed_charas)) {
+            str += currentChar;
+        }
+        else {
             throw SyntaxError("Unexpected character (" + std::string(1, currentChar) + ") while tokenizing\n");
         }
-	}
+    }
 
     if (str.length() > 0) {
         tokens.push_back(str);
     }
 
-	return tokens;
+    return tokens;
 }

8b62e18e4bdeb04c0f16dda3d15c9750ce7e24dc
darren2pro <darren2pro@gmail.com>
2022-10-19 18:14:20 +0800

    Select boolean test cases

---
+++ Team09/Code09/IntegrationTesting/TestBooleanTuple.cpp
@@ -0,0 +1,170 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace IntegrationTesting {
+    TEST_CLASS(TestBooleanTuple) {
+            private:
+            string getCurrentProgram(int ref) {
+                string program1 = "procedure main{\n"
+                                  "        flag = 0;\n" // line 1
+                                  "        call computeCentroid;\n" // line 2
+                                  "        call printResults;\n" // line 3
+                                  "}\n"
+                                  "procedure readPoint{\n"
+                                  "        read x;\n" // line 4
+                                  "        read y;\n" // line 5
+                                  "}\n"
+                                  "procedure printResults{\n"
+                                  "        print flag;\n" // line 6
+                                  "        print cenX;\n" // line 7
+                                  "        print cenY;\n" // line 8
+                                  "        print normSq;\n" // line 9
+                                  "}\n"
+                                  "procedure computeCentroid{\n"
+                                  "        x = y + 100 * 5;\n" // line 10
+                                  "        count = 0;\n" // line 11
+                                  "        cenX = 0;\n" // line 12
+                                  "        cenY = 0;\n" // line 13
+                                  "        call readPoint;\n" // line 14
+                                  "        while ((x != 0) && (y != 0)) {\n" // line 15
+                                  "            count = count + 1;\n" // line 16
+                                  "            cenX = cenX + x;\n" // line 17
+                                  "            cenY = cenY + y;\n" // line 18
+                                  "            call readPoint;\n" // line 19
+                                  "        }\n"
+                                  "        if (count == 0) then {\n" // line 20
+                                  "            flag = 1;\n" // line 21
+                                  "        } else {\n"
+                                  "            cenX = cenX / count;\n" // line 22
+                                  "            cenY = cenY / count;\n" // line 23
+                                  "        }\n"
+                                  "        normSq = cenX * cenX + cenY * cenY;\n" // line 24
+                                  "}";
+                string program2 = "procedure read {\n"
+                                  "    mod1 = print % (var1 + 1 * var2 - var3);\n" // line 1
+                                  "    if (var1 - 6 % 1 == (var2 + 1) * 2) then {\n" // line 2
+                                  "        mod2 = var1 + 1 * 100 - var2;\n" // line 3
+                                  "    } else {\n"
+                                  "        var2 = var2 + 1 % 100 - 30 + 60 * 2 / mod2;\n" // line 4
+                                  "    }\n"
+                                  "}\n"
+                                  "procedure call {\n"
+                                  "    mod3 = var1 + 1 * 100 - var2;\n" // line 5
+                                  "    call read1;\n" // line 6
+                                  "    mod4 = var1 + 1 * 100 - var2;\n" // line 7
+                                  "}";
+                // You can add more program strings here and add more switch cases here
+                switch (ref) {
+                    case 1:
+                        return program1;
+                    case 2:
+                        //! TestPatternMatchFull
+                        return program2;
+                    default:
+                        return "";
+                }
+            }
+
+            public:
+            TEST_METHOD(TestBoolean1) {
+                string program = getCurrentProgram(1);
+                SPAManager spaManager;
+                spaManager.loadSimpleSourceFromProgram(program);
+
+                //! Query 1
+                string query1 = "procedure sppp; \n"
+                                "Select BOOLEAN such that Modifies(sppp, \\\"cenX\\\")";
+                unordered_set <string> results1 = spaManager.query(query1);
+                // Expected results: TRUE
+                Assert::AreEqual(1, (int) results1.size());
+                Assert::IsTrue(results1.find("TRUE") != results1.end());
+            }
+
+            TEST_METHOD(TestBoolean2) {
+                string program = getCurrentProgram(1);
+                SPAManager spaManager;
+                spaManager.loadSimpleSourceFromProgram(program);
+
+                //! Query 2
+                string query2 = "procedure p; \n"
+                                "Select BOOLEAN      such that Modifies(p, \\\"x\\\")";
+                unordered_set <string> results2 = spaManager.query(query2);
+                // Expected results: TRUE
+                Assert::AreEqual(1, (int) results2.size());
+                Assert::IsTrue(results2.find("TRUE") != results2.end());
+            }
+
+            TEST_METHOD(TestBoolean3) {
+                string program = getCurrentProgram(1);
+                SPAManager spaManager;
+                spaManager.loadSimpleSourceFromProgram(program);
+
+                //! Query 3
+                string query3 = "procedure p; variable v; \n"
+                                "Select BOOLEAN such that Modifies(\"readPoint\", v)";
+                unordered_set <string> results3 = spaManager.query(query3);
+                // Expected results: TRUE
+                Assert::AreEqual(1, (int) results3.size());
+                Assert::IsTrue(results3.find("TRUE") != results3.end());
+            }
+
+            TEST_METHOD(TestBoolean4) {
+                string program = getCurrentProgram(1);
+                SPAManager spaManager;
+                spaManager.loadSimpleSourceFromProgram(program);
+
+                //! Query 4
+                string query4 = "procedure p; variable v; call ccc; \n  "
+                                "Select BOOLEAN  such that Modifies(\"printResults\", v)";
+                unordered_set <string> results4 = spaManager.query(query4);
+                // Expected results: FALSE
+                Assert::AreEqual(1, (int) results4.size());
+                Assert::IsTrue(results4.find("FALSE") != results4.end());
+            }
+
+            TEST_METHOD(TestBoolean5) {
+                string program = getCurrentProgram(1);
+                SPAManager spaManager;
+                spaManager.loadSimpleSourceFromProgram(program);
+
+                //! Query 5
+                string query5 = "procedure p; variable v; call ccc; \n  "
+                                "Select BOOLEAN  such that Modifies(\"main\", \"random\")";
+                unordered_set <string> results5 = spaManager.query(query5);
+                // Expected results: FALSE
+                Assert::AreEqual(1, (int) results5.size());
+                Assert::IsTrue(results5.find("FALSE") != results5.end());
+            }
+
+            TEST_METHOD(TestBoolean6) {
+                string program = getCurrentProgram(1);
+                SPAManager spaManager;
+                spaManager.loadSimpleSourceFromProgram(program);
+
+                //! Query 6
+                string query6 = "procedure p; variable BOOLEAN; \n    "
+                                "Select BOOLEAN such that Modifies(\"readPoint\", v)";
+                unordered_set <string> results6 = spaManager.query(query6);
+                // Expected results: x, y
+                Assert::AreEqual(2, (int) results6.size());
+                Assert::IsTrue(results6.find("x") != results6.end());
+                Assert::IsTrue(results6.find("y") != results6.end());
+            }
+
+            TEST_METHOD(TestBoolean7) {
+                string program = getCurrentProgram(1);
+                SPAManager spaManager;
+                spaManager.loadSimpleSourceFromProgram(program);
+
+                //! Query 7
+                string query7 = "procedure p; variable BOOLEAN; call ccc; \n   "
+                                "Select BOOLEAN such that Modifies(\"printResults\", v)";
+                unordered_set<string> results7 = spaManager.query(query7);
+                // Expected results: empty
+                Assert::AreEqual(0, (int) results7.size());
+            }
+    };
+}

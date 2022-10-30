83aff246d14c762b3a82235830468037abc87c36
Sharmaine Cheong <sharmainec@u.nus.edu>
2022-10-30 23:42:51 +0800

    Update pkb unit tests

--- Team09/Code09/UnitTesting/ProgramKnowledgeBaseTests/TestPkb.cpp
+++ Team09/Code09/UnitTesting/ProgramKnowledgeBaseTests/TestPkb.cpp
@@ -11,30 +11,74 @@ namespace UnitTesting {
         TEST_METHOD(TestPkbStorage) {
             PKB::PKBStorage* pkb = new PKB::PKBStorage();
 
-            Assert::IsTrue(pkb->relationIsEmpty(Relation::Follows));
-
-            pkb->storeRelations(Relation::Follows, "1", "2");
-
-            Assert::IsFalse(pkb->relationIsEmpty(Relation::Follows));
-
+            // entities: variable
+            std::unordered_set<std::string> varSet;
+            Assert::IsFalse(pkb->entityContains(Declaration::Variable, "x"));
             pkb->storeEntity(Declaration::Variable, "x");
+            Assert::IsTrue(pkb->entityContains(Declaration::Variable, "x"));
+            Assert::IsFalse(pkb->getEntitySet(Declaration::Variable) == varSet);
+            varSet.insert("x");
+            Assert::IsTrue(pkb->getEntitySet(Declaration::Variable) == varSet);
 
-            //NextT
-            pkb->storeRelations(Relation::Next, "1", "2");
-            pkb->storeRelations(Relation::Next, "2", "3");
-            pkb->storeRelations(Relation::Next, "1", "3");
 
-            std::unordered_set<PKB::NextLine> nextLineSet;
-            std::unordered_set<PKB::PrevLine> prevLineSet;
-            nextLineSet.insert("2");
-            nextLineSet.insert("3");
-            prevLineSet.insert("1");
-            prevLineSet.insert("2");
+            // relations: Follows
+            std::unordered_set<std::string> secondFromFirstSet;
+            std::unordered_set<std::string> firstFromSecondSet;
+            std::unordered_set<std::pair<std::string, std::string>, PKB::pairHash> allFollowsSet;
+            Assert::IsTrue(pkb->relationIsEmpty(Relation::Follows));
+            pkb->storeRelations(Relation::Follows, "1", "2");
+            Assert::IsFalse(pkb->relationIsEmpty(Relation::Follows));
+            Assert::IsTrue(pkb->relationContainsSet(Relation::Follows, "1", "2"));
+            Assert::IsTrue(pkb->relationContainsFirst(Relation::Follows, "1"));
+            Assert::IsFalse(pkb->relationContainsFirst(Relation::Follows, "2"));
+            Assert::IsTrue(pkb->relationContainsSecond(Relation::Follows, "2"));
+            Assert::IsFalse(pkb->relationContainsSecond(Relation::Follows, "1"));
 
-            //Assert::IsTrue(pkb->getNextLineT("1") == nextLineSet);
-            //Assert::IsFalse(pkb->getNextLineT("2") == nextLineSet);
-            //Assert::IsTrue(pkb->getPreviousLineT("3") == prevLineSet);
-            //Assert::IsFalse(pkb->getPreviousLineT("2") == nextLineSet);
+            // getRelationSecondFromFirst
+            Assert::IsFalse(pkb->getRelationSecondFromFirst(Relation::Follows, "1") == secondFromFirstSet);
+            secondFromFirstSet.insert("2");
+            Assert::IsTrue(pkb->getRelationSecondFromFirst(Relation::Follows, "1") == secondFromFirstSet);
+
+            // getRelationFirstFromSecond
+            Assert::IsFalse(pkb->getRelationFirstFromSecond(Relation::Follows, "2") == firstFromSecondSet);
+            firstFromSecondSet.insert("1");
+            Assert::IsTrue(pkb->getRelationFirstFromSecond(Relation::Follows, "2") == firstFromSecondSet);
+
+            pkb->storeRelations(Relation::Follows, "3", "4");
+
+            // getRelationAllSecond
+            Assert::IsFalse(pkb->getRelationAllSecond(Relation::Follows) == secondFromFirstSet);
+            secondFromFirstSet.insert("4");
+            Assert::IsTrue(pkb->getRelationAllSecond(Relation::Follows) == secondFromFirstSet);
+
+            // getRelationAllFirst
+            Assert::IsFalse(pkb->getRelationAllFirst(Relation::Follows) == firstFromSecondSet);
+            firstFromSecondSet.insert("3");
+            Assert::IsTrue(pkb->getRelationAllFirst(Relation::Follows) == firstFromSecondSet);
+
+            // getRelationSet
+            Assert::IsFalse(pkb->getRelationSet(Relation::Follows) == allFollowsSet);
+            allFollowsSet.insert(std::make_pair("1", "2"));
+            allFollowsSet.insert(std::make_pair("3", "4"));
+            Assert::IsTrue(pkb->getRelationSet(Relation::Follows) == allFollowsSet);
+
+
+            // patterns: assign
+            std::unordered_set<PKB::LineNum> lineNo;
+            std::unordered_set<std::pair<PKB::LineNum, PKB::Variable>, PKB::pairHash> lineVarSet;
+            Assert::IsTrue(pkb->getPatternLineByVar(Pattern::Assign, "x") == lineNo);
+            Assert::IsTrue(pkb->getPatternLineByUS(Pattern::Assign) == lineNo);
+            pkb->storePatterns(Pattern::Assign, "x", "1", "v+x*y-z*t");
+            lineNo.insert("1");
+            Assert::IsTrue(pkb->getPatternLineByVar(Pattern::Assign, "x") == lineNo);
+            Assert::IsTrue(pkb->getPatternLineByUS(Pattern::Assign) == lineNo);
+            Assert::IsFalse(pkb->getPatternLineVarSet(Pattern::Assign) == lineVarSet);
+            lineVarSet.insert(std::make_pair("1", "x"));
+            Assert::IsTrue(pkb->getPatternLineVarSet(Pattern::Assign) == lineVarSet);
+
+
+            // cache
+            Assert::IsFalse(pkb->isCacheFullyComputed(Relation::Affects));
         }
     };
 }
